14893b26cf35ba61bffc0d13c884561cc4956b33
FelipeBaeza <felipe.baeza.m@usach.cl>
2025-04-11 01:32:23 -0400

    Creacion de los routers en el front

--- Laboratorio1/DeliveryAppFrontend/src/main.js
+++ Laboratorio1/DeliveryAppFrontend/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router' // Make sure this path is correct
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
 
