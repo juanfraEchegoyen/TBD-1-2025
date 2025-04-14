0bbaca05294049f7106b1be0cd66954a9d087b39
Reinaldo Pacheco Parra <reinaldo.pacheco@usach.cl>
2025-04-14 00:45:10 -0400

    feat(cliente): Implement service and controller for Cliente entity
    Añade la capa de servicio (ClienteService, ClienteServiceImpl) relacionada con la entidad cliente, se hacen test en postman para comprobar funcionamiento.
    
    Implementa el controlador REST (ClienteController) para exponer los endpoints de la API para gestionar clientes:
    - POST /api/v1/clientes: Crear un nuevo cliente.
    - GET /api/v1/clientes/{rut}: Obtener un cliente por su RUT.
    - GET /api/v1/clientes: Obtener todos los clientes.
    - PUT /api/v1/clientes/{rut}: Actualizar un cliente existente.
    - DELETE /api/v1/clientes/{rut}: Eliminar un cliente por su RUT.

---
+++ Laboratorio1/DeliveryAppBackend/src/main/java/com/app/DeliveryApp/services/ClienteService.java
@@ -0,0 +1,13 @@
+package com.app.DeliveryApp.services;
+
+import com.app.DeliveryApp.models.Cliente;
+import java.util.List;
+import java.util.Optional;
+
+public interface ClienteService {
+    Cliente crearCliente(Cliente cliente);
+    Optional<Cliente> obtenerClientePorRut(String rut);
+    List<Cliente> obtenerTodosLosClientes();
+    Optional<Cliente> actualizarCliente(String rut, Cliente clienteActualizado);
+    boolean eliminarCliente(String rut);
+}
---
+++ Laboratorio1/DeliveryAppBackend/src/main/java/com/app/DeliveryApp/services/ClienteServiceImpl.java
@@ -0,0 +1,69 @@
+package com.app.DeliveryApp.services;
+
+import com.app.DeliveryApp.models.Cliente;
+import com.app.DeliveryApp.repositories.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClienteServiceImpl implements ClienteService {
+
+    private final ClienteRepository clienteRepository;
+
+    @Autowired
+    public ClienteServiceImpl(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+    @Override
+    public Cliente crearCliente(Cliente cliente) {
+        if (clienteRepository.findByRut(cliente.getRut()).isPresent()) {
+            throw new IllegalArgumentException("Cliente con RUT " + cliente.getRut() + " ya existente");
+        }
+        return clienteRepository.save(cliente);
+    }
+
+    @Override
+    public Optional<Cliente> obtenerClientePorRut(String rut) {
+
+        return clienteRepository.findByRut(rut);
+    }
+
+    @Override
+    public List<Cliente> obtenerTodosLosClientes() {
+        return clienteRepository.findAll();
+    }
+
+    @Override
+    public Optional<Cliente> actualizarCliente(String rut, Cliente clienteActualizado) {
+        Optional<Cliente> clienteExistenteOpt = clienteRepository.findByRut(rut);
+
+        if (clienteExistenteOpt.isPresent()) {
+            Cliente clienteParaActualizar = clienteExistenteOpt.get();
+            clienteParaActualizar.setRut(rut);
+            clienteParaActualizar.setNombre(clienteActualizado.getNombre());
+            clienteParaActualizar.setTelefono(clienteActualizado.getTelefono());
+            clienteParaActualizar.setDireccion(clienteActualizado.getDireccion());
+            clienteParaActualizar.setComuna(clienteActualizado.getComuna());
+
+            clienteRepository.update(clienteParaActualizar);
+            return Optional.of(clienteParaActualizar);
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public boolean eliminarCliente(String rut) {
+        if (clienteRepository.findByRut(rut).isPresent()) {
+            int filasAfectadas = clienteRepository.deleteByRut(rut);
+            // true si se elimino
+            return filasAfectadas > 0;
+        } else {
+            return false;
+        }
+    }
+}
