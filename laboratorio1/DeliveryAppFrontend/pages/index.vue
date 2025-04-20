e8f339d0ea0c8244a0330abed2c25824c9c026fe
Reinaldo Pacheco Parra <reinaldo.pacheco@usach.cl>
2025-04-20 02:47:13 -0400

    add views in front (login, logout and 3 views)
    Se agregan las vistas y los enlaces en la sidebar para:
    Vistas (mínimo 3):
    Resumen de pedidos por cliente (monto total, número de pedidos).
    Vista de desempeño por repartidor.
    Vista de empresas asociadas con mayor volumen de comida entregados.
    
    ademas de una opcion de login que luego debe ser configurada para mostrarse o no, y el logout dependiendo si el usuario esta logeado o no

--- laboratorio1/DeliveryAppFrontend/pages/index.vue
+++ laboratorio1/DeliveryAppFrontend/pages/index.vue
@@ -1,4 +1,5 @@
-<script></script>
 <template>
-Hola mundo
-</template>
+    <div class="text-3xl font-bold text-primary text-center mt-10">
+      Bienvenido a AltiroDelivery este es el home
+    </div>
+  </template>
