8fc94d8323a3d88302544b96fd53896f8abe77ae
FelipeBaeza <felipe.baeza.m@usach.cl>
2025-04-29 02:10:03 -0400

    creación y conexión  de login con el back

--- Laboratorio1/DeliveryAppBackend/src/main/java/com/app/DeliveryApp/config/SecurityConfig.java
+++ Laboratorio1/DeliveryAppBackend/src/main/java/com/app/DeliveryApp/config/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
