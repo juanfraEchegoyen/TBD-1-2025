9ab990a9ee91c89d4fde08d23787a790c14126cc
Reinaldo Pacheco Parra <reinaldo.pacheco@usach.cl>
2025-04-29 20:54:43 -0400

    Revert "Merge pull request #15 from juanfraEchegoyen/3-create-jwt-login-and-register"
    This reverts commit 6f4c6e5b0f774046ea1b7ac72c8b12b7b65a076c, reversing
    changes made to 6b97f0e044a620a4fbbf7ae6039d63dd5f18b16b.

--- laboratorio1/DeliveryAppFrontend/components/BaseButtonGreen.vue
+++ laboratorio1/DeliveryAppFrontend/components/BaseButtonGreen.vue
@@ -1,20 +1,9 @@
 <template>
   <button
-    class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-    :disabled="disabled"
-    @click="$emit('click')"
+    class="bg-green-700 text-white px-6 py-2 rounded-lg hover:bg-green-800 transition-colors"
+    v-bind="$attrs"
+    v-on="$listeners"
   >
-    <slot></slot>
+    <slot />
   </button>
 </template>
-
-<script setup>
-defineProps({
-  disabled: {
-    type: Boolean,
-    default: false
-  }
-})
-
-defineEmits(['click'])
-</script>
