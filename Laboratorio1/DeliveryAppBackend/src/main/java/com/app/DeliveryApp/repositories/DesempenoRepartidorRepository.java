685d4f2aecf3a9593b29e7c027a27ce70fad80d5
Juanfra Echegoyen <juan.echegoyen@usach.cl>
2025-05-06 20:43:40 -0400

    Las 3 vistas funcionando
    Se corrige error que no dejaba visualizar desempeño repartidor

--- Laboratorio1/DeliveryAppBackend/src/main/java/com/app/DeliveryApp/repositories/DesempenoRepartidorRepository.java
+++ Laboratorio1/DeliveryAppBackend/src/main/java/com/app/DeliveryApp/repositories/DesempenoRepartidorRepository.java
@@ -1,15 +1,21 @@
 package com.app.DeliveryApp.repositories;
+
 import com.app.DeliveryApp.models.views.DesempenoRepartidor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
+
 @Repository
 public class DesempenoRepartidorRepository {
     @Autowired
     private JdbcTemplate jdbcTemplate;
+
     public List<DesempenoRepartidor> findAll() {
-        String sql = "SELECT rut_repartidor, nombre_repartidor, cantidad_pedidos, monto_total FROM vista_desempeno_repartidor";
+        String sql = "SELECT rut_repartidor, nombre_repartidor, entregas_realizadas, puntuacion_promedio " +
+                "FROM vista_desempeno_repartidor";
+
         return jdbcTemplate.query(sql, (rs, rowNum) -> {
             DesempenoRepartidor resumen = new DesempenoRepartidor();
             resumen.setRutRepartidor(rs.getString("rut_repartidor"));
